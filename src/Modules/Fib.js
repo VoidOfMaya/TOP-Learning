7f30aae92d94590f00538c6005b976475fed3b0a
VoidOfMaya <mayaofthevoid@gmail.com>
2025-10-23 10:43:58 +0300

    fibonacci recursion implemented (none recursive tailed);

--- src/Modules/Fib.js
+++ src/Modules/Fib.js
@@ -1,1 +1,14 @@
-console.log('running program')
+/*===>23:10.2025: Fibonacci Recursive<===*/
+
+/*
+fib is 
+*/
+const fib = function (n){
+    if( n <= 2){    
+        return n = 1;
+       
+    }
+    return fib(n-1) + fib(n-2)
+}
+
+console.log(fib(18));

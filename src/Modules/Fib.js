0d8c14d0f7f01d4421cb097d52e23fe8aa9af072
VoidOfMaya <mayaofthevoid@gmail.com>
2025-10-23 11:17:54 +0300

    no message

--- src/Modules/Fib.js
+++ src/Modules/Fib.js
@@ -3,12 +3,28 @@
 /*
 fib is 
 */
-const fib = function (n){
+const fibIter =function (n){
+    let arr = [];
+    for(let i = 0; i >= n; i++){
+        let res = 0;
+        if(res <= 2 ){
+            res = 1    
+            arr.concat(res);
+        }
+        arr.concat((res-1)+(res-2));
+    }
+    return arr
+}
+/*
+const fibRec = function (n){
+
     if( n <= 2){    
         return n = 1;
        
     }
-    return fib(n-1) + fib(n-2)
+    return fib(n-1) + fib(n-2);
 }
+    */
 
-console.log(fib(18));
+console.log(`itretive : ${fibIter(18)}`);
+//console.log(`Recursive: ${fibRec(18)}`);

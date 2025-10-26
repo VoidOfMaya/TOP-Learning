107dba24988a77fec2162dc2288d8541db3c2181
VoidOfMaya <mayaofthevoid@gmail.com>
2025-10-26 10:43:20 +0200

    recursive mergeSort and fibonacci sequens at nth position

--- src/Modules/mergeSort.js
+++ src/Modules/mergeSort.js
@@ -1,17 +1,38 @@
 console.log(`running merge sort;`)
-const array = [5,2,4,3,6,8,-1,7];
+const array = [105, 79, 100, 110];
 
 
-const mergeSort = function(arr, res = []){
-    if(arr.length > 1){
+const mergeSort = function(arr){
+    if(arr.length <= 1){
+        return arr;
+    }else{
         const A = arr.slice(0, arr.length/2);
-        const B = arr.slice(arr.length/2, arr.length + 1);
-        console.log(`first half: ${A}\nsecond Half: ${B}`);
-        mergeSort(A);
-        mergeSort(B);
-        
+        const B = arr.slice(arr.length/2, arr.length);
+        return merge(mergeSort(A), mergeSort(B));     
     }
-    return res
 }
-
-mergeSort(array);
+const merge = function (A, B){
+    let k =[];
+    let Ai = 0;
+    let Bi = 0;
+    while(Ai < A.length && Bi < B.length){
+        if(A[Ai]< B[Bi]){
+            k.push(A[Ai]);
+            Ai++;
+        }else{
+            k.push(B[Bi]);
+            Bi++;
+        }
+    }
+    while (Ai < A.length) {
+        k.push(A[Ai]);
+        Ai++;
+    }
+    while (Bi < B.length) {
+        k.push(B[Bi]);
+        Bi++;
+    }
+    return k
+}
+console.log(`sorted data: ${mergeSort(array)}`);
+console.log(`original data: ${array}`)

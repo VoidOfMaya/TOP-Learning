f2f358fa0ba35d8d349e5b73a65445859eb8b0af
VoidOfMaya <mayaofthevoid@gmail.com>
2025-10-27 12:33:27 +0200

    linkedlist fully function

--- src/Modules/linkedlists.js
+++ src/Modules/linkedlists.js
@@ -1,38 +1,92 @@
 class LinkedList{
-    Length = 0;
-    head = null;
+    #length = 0;
+    #head = null;
     append(value){
         const node =new Node(value)
-        if(this.head === null){
-            this.head = node;
+        if(this.#head === null){
+            this.#head = node;
             
         }else{
-            let current = this.head
+            let current = this.#head
             while(current.next !== null){
                 current = current.next
             }
             current.next = node
         }
-        length++;
+        this.#length++;
     }
     prepend(value){
-        if(this.head ===null){
+        if(this.#head ===null){
             this.append(value);
         }
         else{
             const node =new Node (value);
-            node.next = this.head;
-            this.head = node;
+            node.next = this.#head;
+            this.#head = node;
         }
-        length++;
+        this.#length++;
     }
-
-    toString(){
-        let current = this.head;
+    getSize(){
+        return this.#length;
+    }
+    getHead(){
+        return this.#head;
+    }
+    getTail(){
+        let current = this.#head;
         while(current.next !== null){
-            console.log(`(${current.value})=>`);
+            current= current.next
+        }
+        return current;
+    }
+    getNode(index){
+        let current = this.#head;
+        if( typeof index === "number"&& index < this.#length && index >= 0){
+            for(let i = 0; i < index; i++){
+                current = current.next;
+            }
+            return current;
+        }else{
+            console.warn(`index out of bound, please input a valid index`);
+            return null;
+        }
+    }
+    pop(){
+        if(this.#length ===0) return;
+        if(this.#length ===1){
+            this.#head = null;
+            this.#length--;
+            return
+        }
+        const prev = this.getNode(this.#length-2);
+        
+        prev.next = null;
+        this.#length--;
+    }
+    contains(value){
+        for(let i = 0; i < this.#length; i++){
+            const node =this.getNode(i);
+            if(node.value === value) return true; 
+        }
+        return false;
+    }
+    find(value){
+        for(let i = 0; i < this.#length; i++){
+            const node =this.getNode(i);
+            if(node.value === value){
+                return i
+            }
+        }
+        return null;
+    }
+    toString(){
+        let current = this.#head;
+        let str =''
+        for(let i = 0; i < this.#length; i++){
+            (i < this.#length-1) ? str= str + `(${current.value})->`: str= str + `null`;       
             current = current.next;
         }
+        console.log(str);
     }
 }
 class Node{
--- src/index.js
+++ src/index.js
@@ -48,14 +48,28 @@ import { run } from "./Modules/recursion";
 import { LinkedList } from "./Modules/linkedlists";
 
 const list = new LinkedList ();
+//1
 list.append('cats');
 list.append('dogs');
 list.append('fish');
 list.append('lizards');
 list.append('birds');
-
+//2
+list.prepend('batman');
 list.prepend('chickens');
-
+//3
+list.pop();
+//4
 list.toString();
-
-
+//5
+console.log(`size ; ${list.getSize()}`);
+//6
+console.log(`head ; ${JSON.stringify(list.getHead())}`);
+//7
+console.log(`tail ; ${list.getTail()}`);
+//8
+console.log(`get node: ${list.getNode(1)}`)
+//8
+console.log(list.find('batman')); 
+//9
+console.log(list.contains('lizards'));
